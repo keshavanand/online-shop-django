353e40d7647e8fba518d734bd4cfa78da542a663
keshavanand <keshavanandsingh89@gmail.com>
2024-04-22 22:13:40 -0400

    added async tasks using celery

--- myshop/settings.py
+++ myshop/settings.py
@@ -115,6 +115,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CART_SESSION_ID = 'cart'
