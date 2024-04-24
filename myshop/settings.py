44a54e311392c22991885ecf0fc5960393a5a1ad
keshavanand <keshavanandsingh89@gmail.com>
2024-04-23 22:23:24 -0400

    edited admin site to export csv and pdf for order

--- myshop/settings.py
+++ myshop/settings.py
@@ -148,6 +148,7 @@ CART_SESSION_ID = 'cart'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
